30a39d2b904876288ef50a8e0b5b96489eaa039c
czp3009 <czp3009@gmail.com>
2017-06-02 18:01:35 +0800

    添加 JMenuBar

---
+++ src/main/java/com/hiczp/bilibili/live/danmuji/Config.java
@@ -0,0 +1,8 @@
+package com.hiczp.bilibili.live.danmuji;
+
+/**
+ * Created by czp on 17-6-2.
+ */
+public class Config {
+    public static final String GITHUB_REPOSITORY = "https://github.com/czp3009/danmuji";
+}
